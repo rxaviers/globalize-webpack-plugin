4462e134dd6da7b933f7cc2a400c3c5775c2c4c7
jbellenger <jbellenger@twitter.com>
2017-05-09 14:42:55 -0700

    test string module ids
    - add tests cases for different module id scenarios
    - instead of importing globalize as module 1, get its module id from the
      webpack stats
    - move test output out of test dir, prevents endless loop when running
      `npm run test -- --watch`
    - lint tests

--- test/ProductionModePlugin.js
+++ test/ProductionModePlugin.js
@@ -1,44 +1,55 @@
-var expect = require("chai").expect;
 var GlobalizePlugin = require("../index");
-var path = require("path");
-var webpack = require("webpack");
-var rimraf = require("rimraf");
+var expect = require("chai").expect;
 var fs = require("fs");
+var mkdirp = require("mkdirp");
+var path = require("path");
+var rimraf = require("rimraf");
+var webpack = require("webpack");
 
-var OUTPUT_PATH = path.join(__dirname, "output");
-
-var webpackConfig = {
-  entry: {
-    app: path.join(__dirname, "fixtures/app")
-  },
-  output: {
-    path: OUTPUT_PATH,
-    filename: "app.js"
-  },
-  plugins: [
-    new GlobalizePlugin({
-      production: true,
-      developmentLocale: "en",
-      supportedLocales: ["en"],
-      messages: path.join(__dirname, "fixtures/translations/[locale].json"),
-      output: "[locale].js"
-    }),
-    // new webpack.NamedModulesPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.js',
-      minChunks: function(module, count) {
-        const nodeModules = path.resolve(__dirname, "../node_modules");
-        return module.request && module.request.startsWith(nodeModules);
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime',
-      filename: 'runtime.js',
-      minChunks: Infinity,
-    })
-  ]
+var TEST_CASES = {
+  default: [],
+  named: [ new webpack.NamedModulesPlugin() ],
+  hashed: [ new webpack.HashedModuleIdsPlugin() ]
 };
+
+function outputPath(key, file) {
+  return path.join(__dirname, "../_test-output", key, file || "");
+}
+
+function mkWebpackConfig(key) {
+  return {
+    entry: {
+      app: path.join(__dirname, "fixtures/app")
+    },
+    output: {
+      path: outputPath(key),
+      filename: "app.js"
+    },
+    plugins: TEST_CASES[key].concat([
+      new GlobalizePlugin({
+        production: true,
+        developmentLocale: "en",
+        supportedLocales: ["en"],
+        messages: path.join(__dirname, "fixtures/translations/[locale].json"),
+        output: "[locale].js"
+      }),
+      // new webpack.NamedModulesPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        filename: "vendor.js",
+        minChunks: function(module) {
+          const nodeModules = path.resolve(__dirname, "../node_modules");
+          return module.request && module.request.startsWith(nodeModules);
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "runtime",
+        filename: "runtime.js",
+        minChunks: Infinity
+      })
+    ])
+  };
+}
 
 function promisefiedWebpack(config) {
   return new Promise(function(resolve, reject) {
@@ -53,91 +64,103 @@ function promisefiedWebpack(config) {
 
 describe("Globalize Webpack Plugin", function() {
   describe("Production Mode", function() {
-    describe("Basic app", function() {
-      before(function(done) {
-        rimraf(OUTPUT_PATH, function() {
-          fs.mkdirSync(OUTPUT_PATH);
-          done();
-        });
-      });
+    Object.keys(TEST_CASES).forEach(function(key) {
+      describe(`when using ${key} module ids`, function() {
+        var webpackConfig = mkWebpackConfig(key);
+        var myOutputPath = outputPath(key);
+        var compileStats;
 
-      it("should extract formatters and parsers from basic code", function() {
-        return expect(promisefiedWebpack(webpackConfig)).to.eventually.be.fulfilled.then(function(stats) {
-          var outputFilepath = path.join(OUTPUT_PATH, "en.js");
+        before(function(done) {
+          rimraf(myOutputPath, function() {
+            mkdirp.sync(myOutputPath);
+            promisefiedWebpack(webpackConfig)
+              .catch(done)
+              .then(function (stats) {
+                compileStats = stats;
+                done();
+              });
+          });
+        });
+
+        it("should extract formatters and parsers from basic code", function() {
+          var outputFilepath = path.join(myOutputPath, "en.js");
           var outputFileExists = fs.existsSync(outputFilepath);
           expect(outputFileExists).to.be.true;
           var content = fs.readFileSync(outputFilepath).toString();
           expect(content).to.be.a("string");
         });
-      });
 
-      describe("The compiled bundle", function() {
-        var Globalize;
+        describe("The compiled bundle", function() {
+          var Globalize;
 
-        before(function() {
-          global.window = global;
-          // Hack: Expose __webpack_require__.
-          var runtimeFilePath = path.join(__dirname, "./output/runtime.js");
-          var runtimeContent = fs.readFileSync(runtimeFilePath).toString();
-          fs.writeFileSync(runtimeFilePath, runtimeContent.replace(/(function __webpack_require__\(moduleId\) {)/, "window.__webpack_require__ = $1"));
+          before(function() {
+            global.window = global;
+            // Hack: Expose __webpack_require__.
+            var runtimeFilePath = outputPath(key, "runtime.js");
+            var runtimeContent = fs.readFileSync(runtimeFilePath).toString();
+            fs.writeFileSync(runtimeFilePath, runtimeContent.replace(/(function __webpack_require__\(moduleId\) {)/, "window.__webpack_require__ = $1"));
 
-          // Hack2: Load compiled Globalize
-          require("./output/runtime");
-          require("./output/vendor");
-          require("./output/en");
-          require("./output/app");
-          Globalize = global.__webpack_require__(1);
+            // Hack2: Load compiled Globalize
+            require(outputPath(key, "runtime"));
+            require(outputPath(key, "vendor"));
+            require(outputPath(key, "en"));
+            require(outputPath(key, "app"));
 
-          Globalize.locale("en");
-        });
+            const globalizeModuleStats = compileStats.toJson().modules.find(function (module) {
+              return module.name === "./~/globalize/dist/globalize-runtime.js";
+            });
 
-        after(function() {
-          delete global.window;
-          delete global.webpackJsonp;
-        });
+            Globalize = global.__webpack_require__(globalizeModuleStats.id);
+          });
 
-        it("should include formatDate", function() {
-          var result = Globalize.formatDate(new Date(2017, 3, 15), {datetime: "medium"});
-          // Note, the reason for the loose match below is due to ignore the local time zone differences.
-          expect(result).to.have.string("Apr");
-          expect(result).to.have.string("2017");
-        });
+          after(function() {
+            delete global.window;
+            delete global.webpackJsonp;
+          });
 
-        it("should include formatNumber", function() {
-          var result = Globalize.formatNumber(Math.PI);
-          expect(result).to.equal("3.142");
-        });
+          it("should include formatDate", function() {
+            var result = Globalize.formatDate(new Date(2017, 3, 15), {datetime: "medium"});
+            // Note, the reason for the loose match below is due to ignore the local time zone differences.
+            expect(result).to.have.string("Apr");
+            expect(result).to.have.string("2017");
+          });
 
-        it("should include formatCurrency", function() {
-          var result = Globalize.formatCurrency(69900, "USD");
-          expect(result).to.equal("$69,900.00");
-        });
+          it("should include formatNumber", function() {
+            var result = Globalize.formatNumber(Math.PI);
+            expect(result).to.equal("3.142");
+          });
 
-        it("should include formatMessage", function() {
-          var result = Globalize.formatMessage("like", 0);
-          expect(result).to.equal("Be the first to like this");
-        });
+          it("should include formatCurrency", function() {
+            var result = Globalize.formatCurrency(69900, "USD");
+            expect(result).to.equal("$69,900.00");
+          });
 
-        it("should include formatRelativeTime", function() {
-          var result = Globalize.formatRelativeTime(1, "second");
-          expect(result).to.equal("in 1 second");
-        });
+          it("should include formatMessage", function() {
+            var result = Globalize.formatMessage("like", 0);
+            expect(result).to.equal("Be the first to like this");
+          });
 
-        it("should include formatUnit", function() {
-          var result = Globalize.formatUnit(60, "mile/hour", {form: "short"});
-          expect(result).to.equal("60 mph");
-        });
+          it("should include formatRelativeTime", function() {
+            var result = Globalize.formatRelativeTime(1, "second");
+            expect(result).to.equal("in 1 second");
+          });
 
-        it("should include parseNumber", function() {
-          var result = Globalize.parseNumber("1,234.56");
-          expect(result).to.equal(1234.56);
-        });
+          it("should include formatUnit", function() {
+            var result = Globalize.formatUnit(60, "mile/hour", {form: "short"});
+            expect(result).to.equal("60 mph");
+          });
 
-        it("should include parseDate", function() {
-          var result = Globalize.parseDate("1/2/1982");
-          expect(result.getFullYear()).to.equal(1982);
-          expect(result.getMonth()).to.equal(0);
-          expect(result.getDate()).to.equal(2);
+          it("should include parseNumber", function() {
+            var result = Globalize.parseNumber("1,234.56");
+            expect(result).to.equal(1234.56);
+          });
+
+          it("should include parseDate", function() {
+            var result = Globalize.parseDate("1/2/1982");
+            expect(result.getFullYear()).to.equal(1982);
+            expect(result.getMonth()).to.equal(0);
+            expect(result.getDate()).to.equal(2);
+          });
         });
       });
     });
--- test/fixtures/app.js
+++ test/fixtures/app.js
@@ -2,32 +2,32 @@ var like;
 var Globalize = require( "globalize" );
 
 // Use Globalize to format dates.
-console.log( Globalize.formatDate( new Date(), { datetime: "medium" } ) );
+Globalize.formatDate( new Date(), { datetime: "medium" } );
 
 // Use Globalize to format numbers.
-console.log( Globalize.formatNumber( 12345.6789 ) );
+Globalize.formatNumber( 12345.6789 );
 
 // Use Globalize to format currencies.
-console.log( Globalize.formatCurrency( 69900, "USD" ) );
+Globalize.formatCurrency( 69900, "USD" );
 
 // Use Globalize to get the plural form of a numeric value.
-console.log( Globalize.plural( 12345.6789 ) );
+Globalize.plural( 12345.6789 );
 
 // Use Globalize to format a message with plural inflection.
 like = Globalize.messageFormatter( "like" );
-console.log( like( 0 ) );
-console.log( like( 1 ) );
-console.log( like( 2 ) );
-console.log( like( 3 ) );
+like( 0 );
+like( 1 );
+like( 2 );
+like( 3 );
 
 // Use Globalize to format relative time.
-console.log( Globalize.formatRelativeTime( -35, "second" ) );
+Globalize.formatRelativeTime( -35, "second" );
 
 // Use Globalize to format unit.
-console.log( Globalize.formatUnit( 60, "mile/hour", { form: "short" } ) );
+Globalize.formatUnit( 60, "mile/hour", { form: "short" } );
 
 // Use Globalize to parse a number.
-console.log( Globalize.parseNumber( "12345.6789" ) );
+Globalize.parseNumber( "12345.6789" );
 
 // Use Globalize to parse a date.
-console.log( Globalize.parseDate( "1/2/1982" ) );
+Globalize.parseDate( "1/2/1982" );
