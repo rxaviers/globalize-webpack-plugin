8680e6d87a35ccac6941e671957a878af5396192
Dustin <dustin.steiner@gmail.com>
2020-05-06 09:27:21 +0100

    first try at index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,50 @@
+// Type definitions for globalize-webpack-plugin 3.0.0
+// Project: globalize-webpack-plugin
+// Definitions by: Dustin Steiner <dustin.steiner@gmail.com>
+
+export = GlobalizePlugin;
+
+declare class GlobalizePlugin {
+  constructor(attributes: GlobalizePluginAttributes);
+  
+  apply(compiler: ): void;
+}
+
+declare namespace GlobalizePlugin {
+  type CldrFunction = (locale: string) => object;
+  
+  interface TimeZoneLocation {
+      abbrs: string[];
+      untils: number[];
+      offsets: number[];
+      isdsts: number[];
+    }
+  
+  interface TimeZoneArea {
+    [location: string]: TimeZoneLocation
+  }
+  
+  interface TimeZoneAreaData {
+    [area: string]: TimeZoneArea
+  }
+  
+  interface TimeZoneData {
+    zoneData: TimeZoneAreaData
+  }
+  
+  type TimeZoneDataFunction = () => TimeZoneData;
+  
+  type ModuleFilterFunction = (path: string) => boolean;
+
+  interface GlobalizePluginAttributes {
+    production: boolean;
+    developmentLocale: string;
+    supportedLocales: string[];
+    cldr?: CldrFunction;
+    messages?: string | string[];
+    timeZoneData?: TimeZoneDataFunction;
+    output?: string;
+    moduleFilter: ModuleFilterFunction;
+    tmpdirBase?: string
+  }
+}
